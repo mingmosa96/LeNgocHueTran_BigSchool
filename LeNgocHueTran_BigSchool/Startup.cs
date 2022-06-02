62067cefd386f73c8a6630f3c483e68add6c9358
Lê Ngọc Huế Trân <mk.ngochuetran2001@gmail.com>
2022-06-02 16:04:50 +0700

    Add project files.

---
+++ LeNgocHueTran_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LeNgocHueTran_BigSchool.Startup))]
+namespace LeNgocHueTran_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
